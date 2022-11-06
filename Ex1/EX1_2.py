ee1a41cf98d9da366209262f41be1e274a5f5dad
behnamafrasyabi <115899642+behnamafrasyabi@users.noreply.github.com>
2022-11-06 23:02:46 +0330

    Update EX1_2.py

--- Ex1/EX1_2.py
+++ Ex1/EX1_2.py
@@ -1,1 +1,11 @@
 
+def EX1_2(x,y):
+    '''
+This function can calculate the area by taking the height and base of the triangle and return it
+'''
+
+    if x<0 or y<0:
+        print("the data is not correct")
+    else:
+        s=(x*y)/2
+        return(s)
