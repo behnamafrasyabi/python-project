b1cd8710f67526068029e8bc097ac38d33096442
behnamafrasyabi <115899642+behnamafrasyabi@users.noreply.github.com>
2022-11-06 23:01:35 +0330

    Update EX1_4.py

--- Ex1/EX1_4.py
+++ Ex1/EX1_4.py
@@ -1,1 +1,9 @@
-
+def EX1_4(day):
+    '''
+This function can return the names of the days of the week by getting the number
+'''
+    
+    x=['monday','Tuesday','wednesday','thursday','friday','saturday','sunday']
+    day=day-1
+    return(x[day])
+print(EX1_4(2))
