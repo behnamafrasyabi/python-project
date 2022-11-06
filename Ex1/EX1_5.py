94c527c31732746105bf6bf272e517c96c4da51c
behnamafrasyabi <115899642+behnamafrasyabi@users.noreply.github.com>
2022-11-06 23:00:55 +0330

    Update EX1_5.py

--- Ex1/EX1_5.py
+++ Ex1/EX1_5.py
@@ -1,1 +1,12 @@
-
+def EX1_5(x):
+    '''
+This function can announce the person's life status by taking the person's age
+'''
+    if x<1:
+        return("infant")
+    elif x<13:
+        return("child")
+    elif x<20:
+        return("teenager")
+    else :
+        return("adult")
